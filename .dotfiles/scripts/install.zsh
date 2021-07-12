ae1a6a2540ddc4169930ab2dd69b98cedbb88f95
Ruben Sibon <mail@rubensibon.nl>
2021-07-12 17:18:35 +0200

    Update install.zsh

--- .dotfiles/scripts/install.zsh
+++ .dotfiles/scripts/install.zsh
@@ -1,9 +1,3 @@
-if ! command -v git > /dev/null; then
-  echo "Git is not found. Please install it."
-  echo "See: https://git-scm.com/book/en/v2/Getting-Started-Installing-Git"
-  exit 0
-fi
-
 cd
 
 if ! [[ -d ".dotfiles/.gitrepo" ]]; then

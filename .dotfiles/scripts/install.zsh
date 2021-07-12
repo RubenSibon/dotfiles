00e28b116494a01f1d471ff1a1076505bd15bc5d
Ruben Sibon <mail@rubensibon.nl>
2021-07-12 16:47:57 +0200

    Update install.zsh

--- .dotfiles/scripts/install.zsh
+++ .dotfiles/scripts/install.zsh
@@ -7,7 +7,7 @@ fi
 cd
 
 if ! [[ -d ".dotfiles/.gitrepo" ]]; then
-  git clone --bare --recursive git@github.com:RubenSibon/.dotfiles.git $HOME/.dotfiles/.gitrepo
+  git clone --bare --recursive git@github.com:RubenSibon/dotfiles.git $HOME/.dotfiles/.gitrepo
 else
   echo "'~/.dotfiles/.gitrepo/' already exists. Not cloning the repository again."
   echo "Continuing to update the configuration..."

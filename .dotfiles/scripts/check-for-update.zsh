af6a8113e1e77980b365d3db70dfa8d855c9850a
Ruben Sibon <mail@rubensibon.nl>
2022-01-25 11:00:54 +0100

    Update check-for-update.zsh

--- .dotfiles/scripts/check-for-update.zsh
+++ .dotfiles/scripts/check-for-update.zsh
@@ -1,5 +1,7 @@
-WORK_DIR="${HOME}/.dotfiles/scripts"
-LAST_CHECKED_FILE="${WORK_DIR}/.last_checked"
+WORKDIR="${HOME}/.dotfiles/scripts"
+GITDIR="${HOME}/.dotfiles/.gitrepo"
+LAST_CHECKED_FILE="${WORKDIR}/.last_checked"
+COOLDOWN_HOURS=72
 NOW=$(date +%s)
 LAST_CHECKED=$NOW
 
@@ -9,15 +11,27 @@ else
     echo $(date +%s) > $LAST_CHECKED_FILE 
 fi
 
-if [[ $LAST_CHECKED -lt `expr $NOW - 72 \* 60` ]]; then
+if [[ $LAST_CHECKED -lt `expr $NOW - $COOLDOWN_HOURS \* 60` ]]; then
     CHANGED=0
-
+    
     echo $(date +%s) > $LAST_CHECKED_FILE
     
-    /usr/bin/git --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME remote update && /usr/bin/git --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME status -uno | grep -q 'Your branch is behind' && CHANGED=1
+    # Fetch changes from remote and check if dotfiles branch is behind; set CHANGED to 1 if so.
+    /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME remote update && /usr/bin/git --git-dir=$GITDIR --work-tree=$HOME status -uno | grep -q 'Your branch is behind' && CHANGED=1
     
     if [ $CHANGED = 1 ]; then
-        $WORK_DIR/update.zsh
+        echo "Your dotfiles need to be updated."
+        
+        read -p "Do you wish to update the dotfiles (y/N)? " -n 1 -r
+        echo
+        
+        if [[ $REPLY =~ ^[Yy]$ ]]
+            echo "Starting update..."
+            
+            $WORKDIR/update.zsh
+        else
+            echo "Not updating dotfiles at this time."
+        fi
     else
         echo "Dotfiles are up-to-date."
     fi

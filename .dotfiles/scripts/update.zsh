dd96608e9478b03fbb3f3260ab4b0c9e9dfd6b50
Ruben Sibon <r.sibon@amsterdam.nl>
2023-11-05 14:51:09 +0100

    Install Homebrew if needed; improve update comms

--- .dotfiles/scripts/update.zsh
+++ .dotfiles/scripts/update.zsh
@@ -6,25 +6,39 @@
 /usr/bin/git --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME submodule update --init --recursive
 
 # Install fzf (fuzzy finder)
+echo "\n🤖 Installing/updating fuzzy finder (fzf)..."
 ~/.fzf/install
+echo "✔ done installing/updating fzf.\n"
 
 # Install Vim plugins
 if command -v vim > /dev/null; then
+    echo "🤖 Installing/updating Vundle plugins for Vim..."
     vim +PluginInstall +qall
+    echo "✔ done installing/updating Vundle plugins.\n"
 else
     echo "Vim is not installed. Not installing Vundle plugins."
 fi
 
 # Install Volta, the hassle-free JavaScript Tool Manager
+echo "🤖 Installing/updating Volta..."
 curl https://get.volta.sh | bash
+echo "✔ done installing/updating Volta.\n"
 
 # Update Homebrew on macOS
 if [[ $OSTYPE == 'darwin'* ]]; then
+    if [ ! -f "`which brew`" ]; then
+        echo "🤖 Installing Homebrew..."
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        echo "✔ done installing Homebrew.\n"
+    fi
+
+    echo "🤖 Updating Homebrew..."
     brew update && brew upgrade
+    echo "✔ done updating Homebrew.\n"
 fi
 
 source ~/.zshrc
 
-echo "\nDotfiles install/update is done!\n\nYou might have to restart your terminal emulator."
+echo "🤖 Dotfiles install/update is done!\n\nYou might have to restart your terminal emulator."
 
 chsh -s $(which zsh)

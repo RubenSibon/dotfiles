054562ec6d155e5ecffc527e806ec17deeab5884
Ruben Sibon <r.sibon@amsterdam.nl>
2026-02-13 14:09:55 +0100

    Script to install dev tools on Debian distros

---
+++ .dotfiles/scripts/dev-env-debian.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+sudo apt update && sudo apt upgrade
+sudo apt install build-essential git
--- .dotfiles/scripts/install.zsh
+++ .dotfiles/scripts/install.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+if [ -f "/etc/debian_version" ]; then
+   $HOME/.dotfiles/scripts/dev-env-debian.zsh
+fi
+
 if [ ! -d ".dotfiles/.gitrepo" ]; then
     git clone --bare --recursive https://github.com/RubenSibon/dotfiles.git $HOME/.dotfiles/.gitrepo
 else
@@ -10,4 +14,3 @@ fi
 /usr/bin/git --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME checkout HEAD --force
 
 $HOME/.dotfiles/scripts/update.zsh
-

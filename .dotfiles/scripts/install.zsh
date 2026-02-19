78a3379675e06ebe6785faf5c3825e372e425c29
Ruben Sibon <mail@rubensibon.nl>
2026-02-19 21:09:09 +0000

    Use dynamic git command in install script

--- .dotfiles/scripts/install.zsh
+++ .dotfiles/scripts/install.zsh
@@ -5,12 +5,12 @@ if [ -f "/etc/debian_version" ]; then
 fi
 
 if [ ! -d ".dotfiles/.gitrepo" ]; then
-    git clone --bare --recursive https://github.com/RubenSibon/dotfiles.git $HOME/.dotfiles/.gitrepo
+    $(which git) clone --bare --recursive https://github.com/RubenSibon/dotfiles.git $HOME/.dotfiles/.gitrepo
 else
     echo "'~/.dotfiles/.gitrepo/' already exists."
     echo "Continuing to update the configuration..."
 fi
 
-/usr/bin/git --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME checkout HEAD --force
+$(which git) --git-dir=$HOME/.dotfiles/.gitrepo --work-tree=$HOME checkout HEAD --force
 
 $HOME/.dotfiles/scripts/update.zsh
